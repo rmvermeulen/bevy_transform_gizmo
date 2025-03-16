5e7bcdef3713a5aca653a3c458ce5a7471d5217a
FilePhil <filephil@mail.de>
2025-03-16 17:43:57 +0100

    Removed Transformable from Ground Plane

--- examples/minimal.rs
+++ examples/minimal.rs
@@ -29,7 +29,7 @@ fn setup(
     commands.spawn((
             Mesh3d(meshes.add(Plane3d::default().mesh().size(10.0, 10.0))),
             MeshMaterial3d(materials.add(Color::srgb(0.3, 0.5, 0.3))),
-            GizmoTransformable,
+       //     GizmoTransformable,
         ));
     // cube
     commands.spawn((
